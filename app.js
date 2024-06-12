56d8f8a17408f9c9990c79e27be3c5c05f009929
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-12 21:42:22 +0530

    updated

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 
+const {doctormodel}=require("./models/doctor")
+mongoose.connect("mongodb+srv://nimmyroz:roz206@cluster0.svkepzi.mongodb.net/doctorDB?retryWrites=true&w=majority&appName=Cluster0")
+
 const app=express()
 app.use(cors())
+app.use(express.json())
 
 app.post("/",(req,res)=>{
-    res.send("Done")
+    let input=req.body
+    let doctorapp=new doctormodel(input)
+    console.log(doctorapp)
+    doctorapp.save()
+    res.json({"status":"success"})
 })
 
 app.get("/view",(req,res)=>{
+    doctormodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    )
     res.send("viewed")
 })
 
---
+++ models/doctor.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose")
+const schema=mongoose.Schema(
+    {
+        "name":{type:String,required:true},
+        "cat":{type:String,required:true},
+        "mob":{type:String,required:true},
+        "email":{type:String,required:true},
+        "place":{type:String,required:true},
+        "hname":{type:String,required:true},
+    }
+)
+
+let doctormodel=mongoose.model("doctorapp",schema)
+module.exports={doctormodel}
